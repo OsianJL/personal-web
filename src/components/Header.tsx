1f07141a7e7392df91b4488628bdc0a50efdf2f1
Osián <149536063+OsianJL@users.noreply.github.com>
2024-10-19 11:52:36 +0100

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,17 @@
-import React from 'react';
+import React from 'react'
 import styles from '/src/styles/Header.module.css'
 
 const Header: React.FC = () => {
   return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <a href="">Home</a>
-        <a href="">About Me</a>
-        <a href="">Skills</a>
-        <a href="">Projects</a>
-        <a href="">Experience</a>
-        <a href="">Contact</a>
-      </nav>
-      
-      {/* <h2 className={styles.role}>Musician</h2>
-      <h2 className={styles.role}>Traveler</h2> */}
-      {/* <audio autoPlay loop>
-        <source
-          src="https://res.cloudinary.com/dg4q5s1fc/video/upload/v1729074644/Medicine_Cloud_llxqaz.mp3"
-          type="audio/mpeg"
-        />
-        Your browser does not support the audio element.
-      </audio> */}
-    </header>
-  );
-};
+    <nav className={styles.header}>
+      <a href="">Home</a>
+      <a href="">About Me</a>
+      <a href="">Skills</a>
+      <a href="">Projects</a>
+      <a href="">Experience</a>
+      <a href="">Contact</a>
+    </nav>
+  )
+}
 
-export default Header;
+export default Header
