b756e588a5496e3f752f551e4369b62752d55f46
Osián <149536063+OsianJL@users.noreply.github.com>
2024-10-22 21:35:50 +0100

    intro and navbar responsives

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
-import styles from '/src/styles/Header.module.css'
+import styles from '/src/styles/Header.module.css';
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.header}>
-      <a href='#' className={styles.link} >Home</a>
-      <Link smooth={true} duration={500} className={styles.link} to="about">About Me</Link>
-      <Link smooth={true} duration={500} className={styles.link} to="skills">Skills</Link>
-      <Link smooth={true} duration={500} className={styles.link} to="projects">Projects</Link>
-      <Link smooth={true} duration={500} className={styles.link} to="experience">Experience</Link>
-      <Link smooth={true} duration={500} className={styles.link} to="contact">Contact</Link>
+      <div className={styles.menuToggle} onClick={toggleMenu}>
+        <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`}></div>
+        <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`}></div>
+        <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`}></div>
+      </div>
+      <ul className={`${styles.navList} ${isMenuOpen ? styles.showMenu : ''}`}>
+        <li><a href='#' className={styles.link}>Home</a></li>
+        <li><Link smooth={true} duration={500} className={styles.link} to="about">About Me</Link></li>
+        <li><Link smooth={true} duration={500} className={styles.link} to="skills">Skills</Link></li>
+        <li><Link smooth={true} duration={500} className={styles.link} to="projects">Projects</Link></li>
+        <li><Link smooth={true} duration={500} className={styles.link} to="experience">Experience</Link></li>
+        <li><Link smooth={true} duration={500} className={styles.link} to="contact">Contact</Link></li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
