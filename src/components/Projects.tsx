d0c5890e59044a5c5d0c1a3a0b30bb1e1bee1536
Osián <149536063+OsianJL@users.noreply.github.com>
2024-10-24 19:21:58 +0100

    animation skills

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,11 +8,11 @@ const Projects: React.FC = () => {
     <section className={styles.projects}>
       <h1>Projects</h1>
       <section className={styles.carrousel}>
- <img className={styles.img} src="https://picsum.photos/id/237/200/300" alt="" />
- <img className={styles.img} src="https://picsum.photos/id/237/200/300" alt="" />
- <img className={styles.img} src="https://picsum.photos/id/237/200/300" alt="" />
- <img className={styles.img} src="https://picsum.photos/id/237/200/300" alt="" />
- <img className={styles.img} src="https://picsum.photos/id/237/200/300" alt="" />
+ <img className={styles.img} src="https://picsum.photos/id/227/200/300" alt="" />
+ <img className={styles.img} src="https://picsum.photos/id/228/200/300" alt="" />
+ <img className={styles.img} src="https://picsum.photos/id/229/200/300" alt="" />
+ <img className={styles.img} src="https://picsum.photos/id/230/200/300" alt="" />
+ <img className={styles.img} src="https://picsum.photos/id/231/200/300" alt="" />
       </section>
     </section>
     
