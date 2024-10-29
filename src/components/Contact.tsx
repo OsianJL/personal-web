1261333b3c0381728a19d1e5885f8903509557bd
Osián <149536063+OsianJL@users.noreply.github.com>
2024-10-29 20:18:38 +0000

    textarea

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,34 +1,33 @@
 import React from 'react';
 import styles from '/src/styles/Contact.module.css';
 
-
 const Contact: React.FC = () => {
   return (
     <section className={styles.contact}>
-     <h1>Contact</h1>
-    <form className={styles.contactForm}>
-      <div className={styles.formGroup}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="name">Phone:</label>
-        <input type="text" id="name" name="name" required />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="concept">Concept:</label>
-        <input type="text" id="name" name="name" required />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-      </div>
-      <button type="submit">Send</button>
-    </form>
+      <h1>Contact</h1>
+      <form className={styles.contactForm}>
+        <div className={styles.formGroup}>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="phone">Phone:</label>
+          <input type="text" id="phone" name="phone" required />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="concept">Concept:</label>
+          <input type="text" id="concept" name="concept" required />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required></textarea>
+        </div>
+        <button type="submit">Send</button>
+      </form>
     </section>
   );
 };
